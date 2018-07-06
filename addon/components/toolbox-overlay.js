e31eed8c386d8a459970a640428a424ff2a26b12
Arnaud SJongers <asjongers@gmail.com>
2018-07-06 10:44:13 +0200

    overlay-body type can be set when showing

--- addon/components/toolbox-overlay.js
+++ addon/components/toolbox-overlay.js
@@ -7,8 +7,15 @@ export default Component.extend({
   classNames: "overlay-body",
   classNameBindings: ["isVisible:overlay-body--visible", "error:overlay-body--error", "tooltip:overlay-body--tooltip"],
 
-  error: Ember.computed.equal("type", "error"),
-  tooltip: Ember.computed.equal("type", "tooltip"),
+  toolboxOverlayBody: Ember.inject.service(),
+  type: Ember.computed.alias("toolboxOverlayBody.overlayType"),
+
+  error: Ember.computed("type", "toolboxOverlayBody.types.error", function(){
+    return this.get("type") === this.get("toolboxOverlayBody.types.error");
+  }),
+  tooltip: Ember.computed("type", "toolboxOverlayBody.types.tooltip", function(){
+    return this.get("type") === this.get("toolboxOverlayBody.types.tooltip");
+  }),
 
   click: function(event) {
     if(this.clicked) {
--- addon/services/toolbox-overlay-body.js
+++ addon/services/toolbox-overlay-body.js
@@ -1,11 +1,19 @@
 import Service from '@ember/service';
 
 export default Service.extend({
+  init() {
+    this._super(...arguments);
+    this.set("types", {
+      error: "error",
+      tooltip: "tooltip"
+    })
+  },
   isVisible: false,
   hide: function() {
     this.set("isVisible", false);
   },
-  show: function() {
+  show: function(type) {
+    this.set("overlayType", type);
     this.set("isVisible", true);
   }
 });
